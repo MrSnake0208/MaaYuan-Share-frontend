1511e79a280e01671fd8c05611c1c4054d7f988d
mrsnake <yangpeide0208@gmail.com>
2025-10-31 14:22:51 +0800

    chore: 修改广告栏

--- src/data/ad-banners.ts
+++ src/data/ad-banners.ts
@@ -6,14 +6,14 @@ export interface AdBannerConfigItem {
 
 // 可在此数组中快速增删改广告项
 export const defaultAdBanners: AdBannerConfigItem[] = [
+  // {
+  //   image: '/ad_leidian.webp',
+  //   link: 'https://lddl01.ldmnq.com/downloader/ldplayerinst9.exe?n=LDplayer9_ld_406237_3586_ld.exe',
+  //   alt: '雷电模拟器',
+  // },
   {
-    image: '/ad_leidian.webp',
-    link: 'https://lddl01.ldmnq.com/downloader/ldplayerinst9.exe?n=LDplayer9_ld_406237_3586_ld.exe',
-    alt: '雷电模拟器',
-  },
-  {
-    image: '/android-chrome-512x512.png',
-    link: 'https://mayuan.app/',
+    image: '/周年庆.png',
+    link: 'https://pd.qq.com/s/hismsfiu8',
     alt: 'MaaYuan',
   },
 ]
