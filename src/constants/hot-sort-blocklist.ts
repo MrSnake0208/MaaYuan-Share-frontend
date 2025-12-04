e46e393cd037c645da50b8df51a524d1dc3095c9
root <root@Desktop-QQCYS.>
2025-12-04 18:29:15 +0800

    chore: 更新不显示列表

--- src/constants/hot-sort-blocklist.ts
+++ src/constants/hot-sort-blocklist.ts
@@ -4,10 +4,10 @@ import { Operation } from "models/operation";
 // 将对应的 stageId/levelId 或标题关键字添加到下方列表即可生效（大小写不敏感）。
 export const hotSortHiddenStageIds: string[] = [
   "2_0_2_5_nian_1_1_dong_ku",
-  "	2_0_2_5_nian_1_1",
-  "	2_0_2_5_nian_1_0_yue_dong_ku_you",
+  "2_0_2_5_nian_1_1",
+  "2_0_2_5_nian_1_0_yue_dong_ku_you",
   "2_0_2_5_nian_1_0_yue_dong_ku_zuo",
-  "	2_0_2_5_nian_1_1_dong_ku_01",
+  "2_0_2_5_nian_1_1_dong_ku_01",
 ];
 export const hotSortHiddenLevelIds: string[] = [];
 export const hotSortHiddenTitleKeywords: string[] = [];
